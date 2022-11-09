a680bc9d6b6e18ace4727d02791960c779cd48e5
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-09 20:12:05 +0300

    Created ApiTest

--- src/test/java/com/example/calculatorwebservice/ApiTest.java
+++ src/test/java/com/example/calculatorwebservice/ApiTest.java
@@ -1,10 +1,22 @@
 package com.example.calculatorwebservice;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 public class ApiTest {
+    private static final String BaseUrl = "http://localhost:8080/api/calculate/";
     @Test
-    public void add() {
+    public void GetAdd() {
+        // Arrange
+        double num1 = 2;
+        double num2 = 3;
+        var uri = BaseUrl + "add/" + num1 + "/" + num2;
 
+        // Act
+        var result = RestAssured.when().get(uri);
+
+        // Assert
+        result.then().assertThat().statusCode(200);
     }
 }
