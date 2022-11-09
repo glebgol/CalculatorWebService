80c31b52a3059222140e94c059c1cce5085959a6
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-11-09 10:00:05 +0300

    Added Multiply, Minus, Divide methods

--- src/main/java/com/example/calculatorwebservice/CalculationController.java
+++ src/main/java/com/example/calculatorwebservice/CalculationController.java
@@ -12,6 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculationController {
     @GetMapping("add/{firstArg}/{secondArg}")
     public ResponseEntity<Double> Add(@PathVariable Double firstArg, @PathVariable Double secondArg) {
-        return new ResponseEntity<Double>(firstArg + secondArg, HttpStatus.ACCEPTED);
+        var result = firstArg + secondArg;
+        return new ResponseEntity<Double>(result, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("multiply/{firstArg}/{secondArg}")
+    public ResponseEntity<Double> Multiply(@PathVariable Double firstArg, @PathVariable Double secondArg) {
+        var result = firstArg * secondArg;
+        return new ResponseEntity<Double>(result, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("minus/{firstArg}/{secondArg}")
+    public ResponseEntity<Double> Minus(@PathVariable Double firstArg, @PathVariable Double secondArg) {
+        var result = firstArg - secondArg;
+        return new ResponseEntity<Double>(result, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("divide/{firstArg}/{secondArg}")
+    public ResponseEntity<Double> Divide(@PathVariable Double firstArg, @PathVariable Double secondArg) {
+        if (secondArg == 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        var result = firstArg - secondArg;
+        return new ResponseEntity<Double>(result, HttpStatus.ACCEPTED);
     }
 }
