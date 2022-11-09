4105bb37225988ea9029923584f38e08f40f795c
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-11-09 10:22:17 +0300

    Add Main

--- src/main/java/com/example/calculatorwebservice/Main.java
+++ src/main/java/com/example/calculatorwebservice/Main.java
@@ -1,4 +1,14 @@
 package com.example.calculatorwebservice;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        var facade = new CalculationFacade();
+        System.out.println(facade.Add(1, 1));
+        System.out.println(facade.Multiply(12, 13));
+        System.out.println(facade.Divide(12, 20));
+        System.out.println(facade.Minus(100, 300));
+
+    }
 }
