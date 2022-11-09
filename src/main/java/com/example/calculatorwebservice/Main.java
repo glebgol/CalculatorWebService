a3ccaa0413c516ef80f7edfd78d6540dbe7f7e09
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-11-09 18:28:15 +0300

    Changes

--- src/main/java/com/example/calculatorwebservice/Main.java
+++ src/main/java/com/example/calculatorwebservice/Main.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        var facade = new CalculationFacade();
-        System.out.println(facade.Add(1, 1));
-        System.out.println(facade.Multiply(12, 13));
-        System.out.println(facade.Divide(12, 20));
-        System.out.println(facade.Minus(100, 300));
+        var calculationFacade = new CalculationFacade();
+
+        var sum = calculationFacade.Add(1, 1);
+        var multiplication = calculationFacade.Multiply(12, 13);
+        var division = calculationFacade.Divide(12, 20);
+        var difference = calculationFacade.Minus(100, 300);
+
+        System.out.println(sum);
+        System.out.println(multiplication);
+        System.out.println(division);
+        System.out.println(difference);
     }
 }
